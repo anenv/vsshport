71bc916eaa2bbf45ef62e36d1c78b15ac6d9e8f1
Anenv <anenv@live.cn>
2016-01-16 23:08:06 +0800

    Anenv(www.anenv.com)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vsshport
 centos一键修改ssh端口
+
+wget --no-check-certificate https://raw.githubusercontent.com/Anenv/vsshport/master/sshport.sh
+
+chmod +x sshport.sh
+
+sh sshport.sh
---
+++ sshport.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+###########################################
+#        Centos New SSH Port              #"
+#      Intro: http://www.anenv.com        #
+#      Author: Anenv(anenv@live.cn)       #
+###########################################
+
+clear
+echo ""
+echo "###########################################"
+echo "#        Centos New SSH Port              #"
+echo "#      Intro: http://www.anenv.com        #"
+echo "#      Author: Anenv(anenv@live.cn)       #"
+echo "###########################################"
+echo ""
+
+echo "Input New SSH Port:"
+read sshport
+
+echo 'Port ${sshport}' >> /etc/ssh/sshd_config
+
+iptables -A INPUT -p tcp -m tcp --dport ${sshport} -j ACCEPT
+
+/etc/init.d/iptables restart 
+/etc/init.d/sshd restart 
