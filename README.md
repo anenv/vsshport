ebac7bb073eeba0f318f4d52c505f217427feb7d
Anenv <anenv@live.cn>
2016-10-30 14:12:53 +0800

    Anenv (2016-10-30)

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # vsshport
 centos一键修改ssh端口
 
+## Centos
+
+<pre>
 wget --no-check-certificate https://raw.githubusercontent.com/Anenv/vsshport/master/sshport.sh
-
 chmod +x sshport.sh
-
 sh sshport.sh
+</pre>
--- sshport.sh
+++ sshport.sh
@@ -15,6 +15,11 @@ echo "#      Author: Anenv(anenv@live.cn)       #"
 echo "###########################################"
 echo ""
 
+#Disable SeLinux
+if [ -s /etc/selinux/config ]; then
+	sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+fi
+
 echo "Input New SSH Port:"
 read sshport
 
